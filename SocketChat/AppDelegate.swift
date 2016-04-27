e2da3378f9ebabca9e442d2ee4e7b2595ab14ae5
Cassiszuoan <jerry831210@gmail.com>
2016-04-27 17:53:21 +0800

    Reaching Userlist function done

--- SocketChat/AppDelegate.swift
+++ SocketChat/AppDelegate.swift
@@ -27,6 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        SocketIOManager.sharedInstance.closeConnection()
+        //app 一進入背景就取消連線
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -35,6 +37,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        SocketIOManager.sharedInstance.establishConnection()
+        // 一進入app就建立連線
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
--- SocketIOManager.swift
+++ SocketIOManager.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SocketIOManager: NSObject {
     static let sharedInstance = SocketIOManager()
-    var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://192.168.1.107")!)
+    var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://192.168.1.107:3000")!)
     //By doing this we can simplt write SocketIOManager.shareInstance anywhere around the code and have access to all public properties and methods of the above class
     
     override init(){
@@ -23,6 +23,21 @@ class SocketIOManager: NSObject {
     
     func closeConnection(){
         socket.disconnect()
+    }
+    
+    func connectToServerWithNickName(nickname: String, completionHandler: (userList: [[String: AnyObject]]!) -> Void){
+        socket.emit("connectUser", nickname)
+        // The emit() method of the socket object is what we need for sending any message to the server using the Socket.IO client library
+        
+        // socket.on means the iOs app listening to the server
+        
+        socket.on("userList"){
+            // first paramenter is a NSArray object, where the number of the contained elements depends on the number of results sent by the server. The second parameter is an acknowledge message that can be used to notify the server that the message has been received
+            (dataArray, ack) -> Void in
+            completionHandler(userList: dataArray[0] as! [[String: AnyObject]])
+            // When the user list is received from the server, we call our own method's completion handler passing as an argument that list.
+            
+        }
     }
 }
 
